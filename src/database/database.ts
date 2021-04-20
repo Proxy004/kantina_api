a5c58e57ae4e6ca03e60b38dd2edd6d19aa15777
Proxy004 <paul.j.prenn@gmail.com>
2021-04-20 11:38:29 +0200

    🧐 added database connection

---
+++ src/database/database.ts
@@ -0,0 +1,44 @@
+import mysql from "mysql";
+import { DBQuery } from "../models/DBQuery";
+
+const connectionCredentials: mysql.Pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+const Database: DBQuery = (
+  query: string,
+  params: any,
+  callback: mysql.queryCallback
+) => {
+  connectionCredentials.getConnection(
+    (err: mysql.MysqlError, connection: mysql.PoolConnection) => {
+      if (err) {
+        connection.release();
+        callback(err);
+        throw err;
+      }
+
+      connection.query(
+        query,
+        params,
+        (err: mysql.MysqlError, results, fields) => {
+          connection.release();
+          if (!err) {
+            callback(err, results, fields);
+          } else {
+            callback(err);
+          }
+        }
+      );
+
+      connection.on("error", (err: mysql.MysqlError) => {
+        connection.release();
+        callback(err);
+        throw err;
+      });
+    }
+  );
+};
